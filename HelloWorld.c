60ae87511cb29a1162b8e9f09cfe706277d26e8b
flusherr <muhammedarzum15@gmail.com>
2024-01-03 15:47:07 +0300

    Proje dosyası ekle.

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	int x;
+	x = 123;
+	int y = 321;
+	bool e = false
+	int age = 15;
+	float gpa = 4.02;
+	char grade = 'C';
+	char name[] = "Muhammed Fevzettin";
+
+	printf("Your name is %s\n",name);
+	printf("You are %d years old\n", age);
+	printf("Your average score is %c\n", grade);
+	printf("Your GPA is %f\n",gpa);
+	return 0;
+}
